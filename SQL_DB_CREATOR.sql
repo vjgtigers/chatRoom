da8040775c537ae8952938ce55010d72334a118a
Vaughn Gugger <vjgtigers@gmail.com>
2023-04-16 09:10:34 -0400

    added ip logging when user logs in

--- SQL_DB_CREATOR.sql
+++ SQL_DB_CREATOR.sql
@@ -1,25 +1,25 @@
 CREATE SCHEMA vjgtigers_chatroom;
-CREATE TABLE vjgtigers_chatroom.login_data (
+CREATE TABLE chatroom.login_data (
     login_id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
     client_id int UNIQUE,
     email VARCHAR(45),
     password VARCHAR(200)
 );
-CREATE TABLE vjgtigers_chatroom.banned_ips (
+CREATE TABLE chatroom.banned_ips (
 	banned_ip int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
     ipaddress VARCHAR(45)
 );
-CREATE TABLE vjgtigers_chatroom.client_room (
+CREATE TABLE chatroom.client_room (
 	client_id int,
     room_id int,
 	client_room_id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT
 );
-CREATE TABLE vjgtigers_chatroom.clients (
+CREATE TABLE chatroom.clients (
 	client_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nickname VARCHAR(45),
     active int
 );
-CREATE TABLE vjgtigers_chatroom.history (
+CREATE TABLE chatroom.history (
 	history_id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
     room_id int,
     client_id int,
@@ -28,7 +28,21 @@ CREATE TABLE vjgtigers_chatroom.history (
     
 
 );
-CREATE TABLE vjgtigers_chatroom.rooms (
+CREATE TABLE chatroom.rooms (
 	room_id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
     room_name VARCHAR(45)
+);
+
+CREATE TABLE chatroom.rooms (
+	room_id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+    room_name VARCHAR(45)
+);
+
+CREATE TABLE chatroom.ip_logs (
+	ipLogs_id int NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+    client_id int,
+    ip_add VARCHAR(100),
+    time_date VARCHAR(45)
+    
+
 );
